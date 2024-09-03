74133e80d0da2d5c402b0360a7892d4255c2c685
VIPCodeCoder <vaghasiya1923@students.d211.org>
2024-09-03 09:29:38 -0500

    1.4

--- app/src/main/java/com/example/mobile_apps_2024/Display.kt
+++ app/src/main/java/com/example/mobile_apps_2024/Display.kt
@@ -1,20 +1,78 @@
 package com.example.mobile_apps_2024
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import io.github.jan.supabase.gotrue.auth
+import io.github.jan.supabase.postgrest.from
+import kotlinx.coroutines.withContext
 
 class Display : AppCompatActivity() {
+
+    private lateinit var  displayText:TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_display)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        displayText = findViewById(R.id.display)
+        val logoutButton = findViewById<Button>(R.id.logout_button)
+
+        fetchUserData()
+
+        logoutButton.setOnClickListener{
+            logoutUser()
+        }
+    }
+
+    private fun logoutUser() {
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val response = SupabaseClient.client.auth.signOut()
+                withContext(Dispatchers.Main){
+                    if(response.isSuccessful){
+                        startActivity(Intent(this@Display, LoginActivity::class.java))
+                        finish()
+                    } else {
+                        Toast.makeText(this@Display, "Logout Failed: ${response.error?.message}", Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun fetchUserData() {
+        val user = SupabaseClient.client.auth.currentUserOrNull()
+        if (user != null) {
+            CoroutineScope(Dispatchers.IO).launch {
+                try {
+                    val response = SupabaseClient.client.from("users").select("*").eq("email", user.email).execute()
+                    withContext(Dispatchers.Main) {
+                        if (response.isSuccessful) {
+                            val userData = response.data?.get(0)
+                            val userText = "Welcome ${userData?.get("firstname")} ${userData?.get("lastname")} from ${userData?.get("city")}"
+                            displayText.text = userText
+                        } else {
+                            Toast.makeText(this@Display, "Failed to fetch user data: ${response.error?.message}", Toast.LENGTH_LONG).show()
+                        }
+                    }
+                } catch (e: Exception){
+                    withContext(Dispatchers.Main){
+                        Toast.makeText(this@Display, "Error: ${e.message}", Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
         }
     }
 }
